e1beb4403625aa28ce367a79fde600718ec0ec96
BuzaAlx <buzakivi@gmail.com>
2021-04-21 22:19:23 +0300

    usePopover hook

---
+++ src/hooks/usePopover.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
+function usePopover(userId) {
+  let { currentUser } = useSelector((state) => state.user);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    if (userId && userId !== currentUser.displayName) return;
+    // if (userId !== currentUser.displayName) return;
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return [anchorEl, handleClick, handleClose, open, id, setAnchorEl];
+}
+
+export default usePopover;
